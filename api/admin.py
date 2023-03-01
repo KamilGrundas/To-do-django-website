a34e1343318f78a7d5be3ebb2a3b4a659a9b8d6d
Kamil Grundas <kamil.grundas@gmail.com>
2023-03-01 15:10:34 +0100

    Updated database

--- api/admin.py
+++ api/admin.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User, Task, Team, Room, Team_task
+from .models import User
 
 
 admin.site.register(User)
-admin.site.register(Room)
-admin.site.register(Task)
-admin.site.register(Team)
-admin.site.register(Team_task)
